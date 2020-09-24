80d1deca04f3c310ef05595679a6bc5b2717b6b0
milaobradovic <sofija.obradovic16@gmail.com>
2020-09-24 20:58:13 +0200

    Test.java

--- DrugiTest/Test.java
+++ DrugiTest/Test.java
@@ -1,5 +1,19 @@
 package DrugiTest;
 
-public class Test {
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 
+public class Test {
+	private WebDriver driver;
+
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver",
+				"driver-lib\\chromedriver.exe");
+		
+		this.driver = new ChromeDriver();
+	}	
 }
